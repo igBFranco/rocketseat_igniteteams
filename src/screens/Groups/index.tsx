62a08bfb0674ad4dfa3e220384aa332aaad44024
igBFranco <igor.franco@aluno.unc.br>
2022-10-31 10:20:58 -0300

    Configuração Inicial

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+export function Groups() {
+  return (
+    <SafeAreaView>
+        <Text>Groups</Text>  
+    </SafeAreaView>
+  );
+}
